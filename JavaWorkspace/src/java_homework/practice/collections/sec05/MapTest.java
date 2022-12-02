7679869ef6f55e6ee40d7605ba7569564611033e
Dong-Jae Oh <isf1999@naver.com>
2022-12-02 19:04:43 +0900

    .

--- JavaWorkspace/src/java_homework/practice/collections/sec05/MapTest.java
+++ JavaWorkspace/src/java_homework/practice/collections/sec05/MapTest.java
@@ -5,6 +5,12 @@ import java.util.*;
 public class MapTest {
 
 	public static void main(String[] args) {
+		Map<String, Student> st = new HashMap<String, Student>();
+		st.put("2009001", new Student(2009001, "오동재"));
+		st.put("2009002", new Student(2009002, "박지은"));
+		st.put("2009003", new Student(2009003, "왕이보"));
+		
+		System.out.println(st);
 		
 	}
 
