e2549a7749257e27c82d3e0bb0f1e089f20a1464
Dong-Jae Oh <isf1999@naver.com>
2022-12-16 10:59:29 +0900

    .

--- JavaWorkspace/src/this_is_java_2ndReading/ch07/sec08/exam01/KumhoTire.java
+++
@@ -1,9 +0,0 @@
-package this_is_java_2ndReading.ch07.sec08.exam01;
-
-public class KumhoTire extends Tire {
-	@Override
-	public void roll() {
-		System.out.println("금호 타이어가 회전합니다.");
-	}
-
-}
--- JavaWorkspace/src/this_is_java_2ndReading/ch07/sec08/exam01/Tire.java
+++
@@ -1,7 +0,0 @@
-package this_is_java_2ndReading.ch07.sec08.exam01;
-
-public class Tire {
-	public void roll() {
-		System.out.println("회전합니다.");
-	}
-}
