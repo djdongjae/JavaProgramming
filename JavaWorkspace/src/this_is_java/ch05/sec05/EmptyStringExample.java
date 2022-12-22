a2e4af4ed53c82aa27e5b8e1b891e61a81db41ef
Dong-Jae Oh <isf1999@naver.com>
2022-12-22 10:56:16 +0900

    no message

---
+++ JavaWorkspace/src/this_is_java/ch05/sec05/EmptyStringExample.java
@@ -0,0 +1,14 @@
+package this_is_java.ch05.sec05;
+
+public class EmptyStringExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hobby = "";
+		if(hobby.equals("")) {
+			System.out.println("hobby 변수가 참조하는 String 객체는 빈 문자열");
+		}
+
+	}
+
+}
