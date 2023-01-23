45b6bc15c8d24e23a59f98787f87c69fbe1dd1bb
Dong-Jae Oh <isf1999@naver.com>
2023-01-23 19:57:09 +0900

    .

---
+++ JavaWorkspace/src/this_is_java_1회독/ch08/sec02/RemoteControl.java
@@ -0,0 +1,7 @@
+package this_is_java_1회독.ch08.sec02;
+
+public interface RemoteControl {
+	
+	public void turnOn();
+
+}
---
+++ JavaWorkspace/src/this_is_java_1회독/ch08/sec04/RemoteControl.java
@@ -0,0 +1,11 @@
+package this_is_java_1회독.ch08.sec04;
+
+public interface RemoteControl {
+	
+	int MAX_VOLUME = 10;
+	int MIN_VOLUME = 0;
+	
+	void turnOn();
+	void turnOff();
+
+}
