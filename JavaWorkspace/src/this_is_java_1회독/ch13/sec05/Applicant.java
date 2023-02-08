44daf95997a28429ac01a1bf5d6624756821281d
Dong-Jae Oh <isf1999@naver.com>
2023-02-08 14:59:11 +0900

    no message

---
+++ JavaWorkspace/src/this_is_java_1회독/ch13/sec05/Applicant.java
@@ -0,0 +1,11 @@
+package this_is_java_1회독.ch13.sec05;
+
+public class Applicant<T> {
+	
+	public T kind;
+	
+	public Applicant(T kind) {
+		this.kind = kind;
+	}
+
+}
---
+++ JavaWorkspace/src/this_is_java_1회독/ch13/sec05/Person.java
@@ -0,0 +1,21 @@
+package this_is_java_1회독.ch13.sec05;
+
+public class Person {
+
+}
+
+class Worker extends Person {
+	
+}
+
+class Student extends Person {
+	
+}
+
+class HighStudent extends Student {
+	
+}
+
+class MiddleStudent extends Student {
+	
+}
