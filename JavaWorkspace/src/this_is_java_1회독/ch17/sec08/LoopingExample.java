c05811d6017797c1dc663737cd9b22b41275917d
Dong-Jae Oh <isf1999@naver.com>
2023-02-26 18:25:03 +0900

    no message

---
+++ JavaWorkspace/src/this_is_java_1회독/ch17/sec08/LoopingExample.java
@@ -0,0 +1,28 @@
+package this_is_java_1회독.ch17.sec08;
+
+import java.util.Arrays;
+
+public class LoopingExample {
+	
+	public static void main(String[] args) {
+		int[] intArr = {1, 2, 3, 4, 5};
+		
+		// 잘못 작성한 경우
+		Arrays.stream(intArr)
+		.filter(a -> a%2==0)
+		.peek(n -> System.out.println(n));
+		
+		// 중간 처리 메소드 peek()을 이용해서 반복 처리
+		int total = Arrays.stream(intArr)
+				.filter(a -> a%2 == 0)
+				.peek(n -> System.out.println(n))
+				.sum();
+		System.out.println("총합: " + total + "\n");
+		
+		// 최종 처리 메소드 forEach()를 이용해서 반복 처리
+		Arrays.stream(intArr)
+		.filter(a -> a%2 == 0)
+		.forEach(n -> System.out.println(n));
+	}
+
+}
