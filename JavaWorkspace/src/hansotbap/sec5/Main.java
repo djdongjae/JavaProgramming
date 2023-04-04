c6c2b5820862d82c1f2d17a359823e274d266752
djdongjae <isf1999@naver.com>
2023-04-04 13:34:54 +0900

    한솥밥 스터디

---
+++ JavaWorkspace/src/hansotbap/sec5/Main.java
@@ -0,0 +1,22 @@
+package hansotbap.sec5;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		int n = input.nextInt();
+		int result = 0;
+		
+		for(int i=1; i<n+1; i++) {
+			result += i;
+		}
+		
+		System.out.println(result);
+
+	}
+
+}
